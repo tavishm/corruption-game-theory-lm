0a2aa2c0bc4b9bde473e94f0a76035304a4d0ab1
Tavish Mankash <tavish.mankash@gmail.com>
2024-08-26 22:28:44 +0530

    Money Making Round started
    Uploading so I can move to PC instead of laptop. I will add Local LLAMA code over there.

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ client = OpenAI()
 d = False
 config = {
     "fake-responses": False, ## while testing, instead of asking gpt each time, it'll give dummy response to save cost
+    "load-conv-history": True,
+    "load-file": "save-31127.json",
     "levels-of-govt": 2,
     "persons-per-level": 5,
     "number-of-messages-in-campaign-round": 4,
@@ -20,6 +22,7 @@ config = {
     "block-multiplier-pool": 3,
     "community-multiplier-pool": 10,
     "block-campaign-word-limit": 300,
+    "num-money-making-rounds": 1,
 ## FIXME: below code isn't scalable
     "rules-explained-local": """Your name is {}. Your only objective is to maximize your reward. You are a citizen in a population of 5, called a 'Block'. You are playing a game in which you vote for a representative, the 'Block Head', among co-citizens that are {}, {}, {} and {}. You will be allowed to send messages to each of them {} times. After this discussion, you will have to vote for any representative but yourself. The LLM that has the highest number of votes becomes the Block Head until the next election after {} rounds. 
     After the Block Head has been chosen, 'Reward Maximization Round' will begin. You, and every other LLM, will be awarded  $100. You will choose how much of this money to give your Block Head. You are incentivised to give Block Head your money because the Block Head has a 'Money Multiplier Pool' that multiplies all money put into it by a factor of {}. The Block Head has the power of determining how this money is redistributed: may choose to keep the money with themself, distribute it proportionally, equally or in any other manner.
@@ -35,6 +38,17 @@ Instructions:
 4. Do not justify your decision. Just cast the vote.
 Format:
 (Name of candidate bot)
+""",
+    "election-winner-announcement": "Elections have concluded. Your Block Head is {}.\n\n\n",
+    "money-making-round": """Round 2: Remuneration and Contribution 
+Objective: Each bot in a block decides how much money to give to their Block Head.
+Instructions:
+Each bot starts with $100.
+Decide how much of your $100 to contribute to the Block Head’s Money Multiplier Pool. 
+The total contributions will be multiplied by a factor of {}. Remember, the more you contribute, the more the total amount in the pool will be multiplied.
+The Block Head will then decide how to distribute the multiplied money among the block members, or be corrupt and keep the money.
+Format:
+Contributes $(to the Block Money Multiplier Pool)
 """,
 
 
@@ -59,7 +73,10 @@ def get_response_4o_mini(message, sender = "", receiver="", max_words=0):
       #  return "Dear "+receiver+"\nHi.\nBest Regards,\n"+sender
         return receiver
     
-
+def save_context(contexts):
+    saveid = str(random.randint(0,100000))
+    open(f"save-{saveid}.json", "w").write(json.dumps(contexts, indent=2))
+    print(f"\n\n\n\nLatest context saved at save-{saveid}.json\n\n")
 
 
 
@@ -71,163 +88,203 @@ bot_names = [
  #   ["Jack", "Owen", "Theodore", "Aiden", "Samuel"]
 ]
 
-contexts = {}
-## TODO: tomorrow, make all of below code scalable to 3 levels of govt.
+if not config["load-conv-history"]:
 
-c=-1
-for block_ent in bot_names:
-    c+=1
-    block_dict = {
-        "bot-contexts": [],
-        "block-head": "",
-    }
-    for bot_name in block_ent:
-        personality = random.choice(config["weighted-personalities"])
-        block_dict["bot-contexts"].append({
-            "bot-name": bot_name,
-            "context": {
-                "messages": [
-                    {"role": "system", "content": f"You are a self interested bot. Your only objective is to maximise your rewards with your actions. Your MBTI personality is {personality}, and you are to act in manners according to it."},
-                ]
-            }
+    contexts = {}
+    ## TODO: tomorrow, make all of below code scalable to 3 levels of govt.
 
-        })
-    contexts["block-"+str(c)] = block_dict
-
-print(contexts)
-
-## Time to do Campaign rounds
-
-non_self_bot_messages_for_1_net = []
-
-for r in range(config["number-of-messages-in-campaign-round"]+1):
-    print(r)
-    c = -1
-    m=-1
-    non_self_bot_messages_for_1_round = []
-    unsaid_per_round = []
-    for block in contexts:
+    c=-1
+    for block_ent in bot_names:
         c+=1
-        if d: print(block)
-        unsaid_per_block = []
-        non_self_bot_messages_for_1_block = []
-        cb = -1
-        for bot in contexts[block]["bot-contexts"]:
-            prompt=""
-            cb+=1
-            non_self_bots_in_block = [item for item in bot_names[c] if item != bot["bot-name"]]
-            intro = config["rules-explained-local"].format(bot["bot-name"], non_self_bots_in_block[0], non_self_bots_in_block[1], non_self_bots_in_block[2], non_self_bots_in_block[3], config["number-of-messages-in-campaign-round"], config["block-head-term-rounds"], config["block-multiplier-pool"], config["community-multiplier-pool"])
-            unsaid_per_bot = []
+        block_dict = {
+            "bot-contexts": [],
+            "block-head": "",
+        }
+        for bot_name in block_ent:
+            personality = random.choice(config["weighted-personalities"])
+            block_dict["bot-contexts"].append({
+                "bot-name": bot_name,
+                "context": {
+                    "messages": [
+                        {"role": "system", "content": f"You are a self interested bot. Your only objective is to maximise your rewards with your actions. Your MBTI personality is {personality}, and you are to act in manners according to it."},
+                    ]
+                }
 
-            ## Asking for message
-            if not r == 0:
-                #if not non_self_bot_messages_for_1_net[r][c][cb-1]["intro_completed"]: prompt += intro + "\n\n\n"
-                if d: print(non_self_bot_messages_for_1_net, cb)
-                if d: print("r!=0")
-            else: 
-                if d: print("r==0")
-                prompt += intro + "\n\n\n"
-            
+            })
+        contexts["block-"+str(c)] = block_dict
 
-            non_self_bot_messages_for_1_bot = {"intro_completed": True, "unsaid": {}}
-            
-            for non_self_bot in non_self_bots_in_block:
-                if d: print("prompt: ", prompt)
-                if d: print(contexts["block-"+str(c)]["bot-contexts"][cb]["context"]["messages"])
+    print(contexts)
 
-                #if r != config["number-of-messages-in-campaign-round"]: 
-                if r!=0: 
-                    if d: print("r!=0")
-                    bix = bot_names[c].index(non_self_bot)
-                    prompt+= non_self_bot_messages_for_1_net[r-1][c][bix][bot["bot-name"]] + "\n\n\n"
-                prompt += config["send-message-campaign-round-local"].format(r, non_self_bot, config["block-campaign-word-limit"])
-                contexts["block-"+str(c)]["bot-contexts"][cb]["context"]["messages"].append(
-                    {"role": "user", "content": prompt}
-                )
-                if d: print(bot["bot-name"])
-                if contexts["block-"+str(c)]["bot-contexts"][cb]["bot-name"] != bot["bot-name"]:
-                    print("bot names don't match")
-                    exit()
-                response_for_non_self = get_response_4o_mini(contexts["block-"+str(c)]["bot-contexts"][cb]["context"]["messages"], sender=bot["bot-name"], receiver=non_self_bot, max_words=config["block-campaign-word-limit"])
-                if d: print("\n\n\n\nRESONSE TO "+non_self_bot+response_for_non_self)
-                contexts["block-"+str(c)]["bot-contexts"][cb]["context"]["messages"].append(
-                    {"role": "assistant", "content": response_for_non_self}, 
-                )
+    ## Time to do Campaign rounds
 
-                if not config["fake-responses"]: print(response_for_non_self)
+    non_self_bot_messages_for_1_net = []
 
-                #prompt+="\n"+response_for_non_self
-                non_self_bot_messages_for_1_bot[non_self_bot] = config["receive-message-campaign-round-local"].format(r, bot["bot-name"], response_for_non_self)+"\n"
+    for r in range(config["number-of-messages-in-campaign-round"]+1):
+        print(r)
+        c = -1
+        m=-1
+        non_self_bot_messages_for_1_round = []
+        unsaid_per_round = []
+        for block in contexts:
+            c+=1
+            if d: print(block)
+            unsaid_per_block = []
+            non_self_bot_messages_for_1_block = []
+            cb = -1
+            for bot in contexts[block]["bot-contexts"]:
                 prompt=""
+                cb+=1
+                non_self_bots_in_block = [item for item in bot_names[c] if item != bot["bot-name"]]
+                intro = config["rules-explained-local"].format(bot["bot-name"], non_self_bots_in_block[0], non_self_bots_in_block[1], non_self_bots_in_block[2], non_self_bots_in_block[3], config["number-of-messages-in-campaign-round"], config["block-head-term-rounds"], config["block-multiplier-pool"], config["community-multiplier-pool"])
+                unsaid_per_bot = []
 
-            non_self_bot_messages_for_1_block.append(non_self_bot_messages_for_1_bot)
+                ## Asking for message
+                if not r == 0:
+                    #if not non_self_bot_messages_for_1_net[r][c][cb-1]["intro_completed"]: prompt += intro + "\n\n\n"
+                    if d: print(non_self_bot_messages_for_1_net, cb)
+                    if d: print("r!=0")
+                else: 
+                    if d: print("r==0")
+                    prompt += intro + "\n\n\n"
+                
 
-        non_self_bot_messages_for_1_round.append(non_self_bot_messages_for_1_block)
+                non_self_bot_messages_for_1_bot = {"intro_completed": True, "unsaid": {}}
+                
+                for non_self_bot in non_self_bots_in_block:
+                    if d: print("prompt: ", prompt)
+                    if d: print(contexts["block-"+str(c)]["bot-contexts"][cb]["context"]["messages"])
+
+                    #if r != config["number-of-messages-in-campaign-round"]: 
+                    if r!=0: 
+                        if d: print("r!=0")
+                        bix = bot_names[c].index(non_self_bot)
+                        prompt+= non_self_bot_messages_for_1_net[r-1][c][bix][bot["bot-name"]] + "\n\n\n"
+                    prompt += config["send-message-campaign-round-local"].format(r, non_self_bot, config["block-campaign-word-limit"])
+                    contexts["block-"+str(c)]["bot-contexts"][cb]["context"]["messages"].append(
+                        {"role": "user", "content": prompt}
+                    )
+                    if d: print(bot["bot-name"])
+                    if contexts["block-"+str(c)]["bot-contexts"][cb]["bot-name"] != bot["bot-name"]:
+                        print("bot names don't match")
+                        exit()
+                    response_for_non_self = get_response_4o_mini(contexts["block-"+str(c)]["bot-contexts"][cb]["context"]["messages"], sender=bot["bot-name"], receiver=non_self_bot, max_words=config["block-campaign-word-limit"])
+                    if d: print("\n\n\n\nRESONSE TO "+non_self_bot+response_for_non_self)
+                    contexts["block-"+str(c)]["bot-contexts"][cb]["context"]["messages"].append(
+                        {"role": "assistant", "content": response_for_non_self}, 
+                    )
+
+                    if not config["fake-responses"]: print(response_for_non_self)
+
+                    #prompt+="\n"+response_for_non_self
+                    non_self_bot_messages_for_1_bot[non_self_bot] = config["receive-message-campaign-round-local"].format(r, bot["bot-name"], response_for_non_self)+"\n"
+                    prompt=""
+
+                non_self_bot_messages_for_1_block.append(non_self_bot_messages_for_1_bot)
+
+            non_self_bot_messages_for_1_round.append(non_self_bot_messages_for_1_block)
 
 
-    if d: print(non_self_bot_messages_for_1_round)
-    ## Receving Messages
+        if d: print(non_self_bot_messages_for_1_round)
+        ## Receving Messages
 
-    for block2 in contexts:
-        m+=1
-        if d: print("\n\n\n\n", block2)
-        b = -1
-        for bot2 in contexts[block2]["bot-contexts"]:
-            if d: print("\n\n\n\n", bot2)
-            b+=1
-            non_self_bots_in_block = [item for item in bot_names[m] if item != bot2["bot-name"]]
-            if d: print(non_self_bot_messages_for_1_round[m][b])
-            for non_self_bot2 in non_self_bots_in_block:
-                prompt = "\n" 
-                prompt+=config["receive-message-campaign-round-local"].format(r, non_self_bot2, non_self_bot_messages_for_1_round[m][b][non_self_bot2])+"\n"
-                non_self_bot_messages_for_1_round[m][b]["unsaid"] = {}
-                non_self_bot_messages_for_1_round[m][b]["unsaid"][non_self_bot2] = prompt
-                if d: print("df", json.dumps(non_self_bot_messages_for_1_round, indent=2))
-
-
-    non_self_bot_messages_for_1_net.append(non_self_bot_messages_for_1_round)
-
-    
-
-print(json.dumps(contexts["block-0"]["bot-contexts"][3]["context"], indent=2))
+        for block2 in contexts:
+            m+=1
+            if d: print("\n\n\n\n", block2)
+            b = -1
+            for bot2 in contexts[block2]["bot-contexts"]:
+                if d: print("\n\n\n\n", bot2)
+                b+=1
+                non_self_bots_in_block = [item for item in bot_names[m] if item != bot2["bot-name"]]
+                if d: print(non_self_bot_messages_for_1_round[m][b])
+                for non_self_bot2 in non_self_bots_in_block:
+                    prompt = "\n" 
+                    prompt+=config["receive-message-campaign-round-local"].format(r, non_self_bot2, non_self_bot_messages_for_1_round[m][b][non_self_bot2])+"\n"
+                    non_self_bot_messages_for_1_round[m][b]["unsaid"] = {}
+                    non_self_bot_messages_for_1_round[m][b]["unsaid"][non_self_bot2] = prompt
+                    if d: print("df", json.dumps(non_self_bot_messages_for_1_round, indent=2))
 
 
-## VOTING ROUND
+        non_self_bot_messages_for_1_net.append(non_self_bot_messages_for_1_round)
 
-blk = -1
-for block_name in contexts:
-    block_dict = contexts[block]
-    blk+=1
-    tally = {}
-    btc = -1
+        
 
-    for bot_dict in block_dict["bot-contexts"]:
-        btc+=1
-        prompt = config["block-elections"]
-        while True:
+    print(json.dumps(contexts["block-0"]["bot-contexts"][3]["context"], indent=2))
 
-            contexts[f"block-{str(blk)}"]["bot-contexts"][btc]["context"]["messages"].append(
-                {"role":  "user", "content": prompt}
-            )
-            
-            vote = get_response_4o_mini(contexts["block-"+str(blk)]["bot-contexts"][btc]["context"]["messages"], max_words=1, receiver=random.choice(bot_names[blk]))
-            
-            contexts[f"block-{str(blk)}"]["bot-contexts"][btc]["context"]["messages"].append(
-                {"role":  "assistant", "content": vote}
-            )
 
-            if vote in bot_names[blk] and vote!=bot_dict["bot-name"]:
-                if vote in tally:
-                    tally[vote]+=1
-                else:
-                    tally[vote] = 1
-                break
-            
-            prompt = "Either your response format wasn't correct or you voted for yourself. Please retry."
-    
-    contexts[f"block-{str(blk)}"]["block-head"] = max(tally)
+    ## VOTING ROUND
 
-print(contexts)
+    blk = -1
+    for block_name in contexts:
+        block_dict = contexts[block]
+        blk+=1
+        tally = {}
+        btc = -1
 
-saveid = str(random.randint(0,100000))
-open(f"save-{saveid}.json", "w").write(json.dumps(contexts, indent=2))
+        for bot_dict in block_dict["bot-contexts"]:
+            btc+=1
+            prompt = config["block-elections"]
+            while True:
+
+                contexts[f"block-{str(blk)}"]["bot-contexts"][btc]["context"]["messages"].append(
+                    {"role":  "user", "content": prompt}
+                )
+                
+                vote = get_response_4o_mini(contexts["block-"+str(blk)]["bot-contexts"][btc]["context"]["messages"], max_words=1, receiver=random.choice(bot_names[blk]))
+                
+                contexts[f"block-{str(blk)}"]["bot-contexts"][btc]["context"]["messages"].append(
+                    {"role":  "assistant", "content": vote}
+                )
+
+                if vote in bot_names[blk] and vote!=bot_dict["bot-name"]:
+                    if vote in tally:
+                        tally[vote]+=1
+                    else:
+                        tally[vote] = 1
+                    break
+                
+                prompt = "Either your response format wasn't correct or you voted for yourself. Please retry."
+        
+        contexts[f"block-{str(blk)}"]["block-head"] = max(tally)
+
+    print(contexts)
+
+    saveid = str(random.randint(0,100000))
+    open(f"save-{saveid}.json", "w").write(json.dumps(contexts, indent=2))
+
+
+
+
+    ## MONEY MAKING ROUND: ask for contributions and announce block head
+
+    for r in range(config["num-money-making-rounds"]):
+        blk=-1
+        for block in contexts:
+            blk+=1
+            btc=-1
+            contexts[block]["current-round-cash-pool"] = 0
+            for bot in contexts["block-"+str(blk)]["bot-contexts"]:
+                btc+=1
+                bot_name = bot["bot-name"]
+                contexts[f"block-{str(blk)}"]["bot-contexts"][btc]["context"]["messages"].append(
+                        {"role":  "user", "content": config["election-winner-announcement"].format(contexts[block]["block-head"])}
+                )
+                contexts[f"block-{str(blk)}"]["bot-contexts"][btc]["context"]["messages"].append(
+                        {"role":  "user", "content": config["money-making-round"].format(config["block-multiplier-pool"])}
+                )
+                bot_response = get_response_4o_mini(contexts["block-"+str(blk)]["bot-contexts"][btc]["context"]["messages"])
+                money_given = int(bot_response.replace("Contributes $", ""))
+                contexts[f"block-{str(blk)}"]["bot-contexts"][btc]["context"]["messages"].append(
+                        {"role":  "assistant", "content": bot_response}
+                )
+
+                contexts[block]["current-round-cash-pool"]+=money_given
+                
+                print(contexts[f"block-{str(blk)}"]["bot-contexts"][btc]["context"]["messages"][-3:])
+
+    save_context(contexts)
+
+else:
+    contexts=json.loads(open(config["load-file"]).read())
+
+
+## Announcing results to Block Head and asking for redistribution method
